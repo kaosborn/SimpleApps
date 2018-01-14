936d049a865df700b5b4ab3ec10d82e55f9c9183
kaosborn <kaosborn@users.noreply.github.com>
2018-01-13 21:07:10 -0800

    rework for explicit binding update

--- WcWpfMvvm/View/WcWindow.xaml.cs
+++ WcWpfMvvm/View/WcWindow.xaml.cs
@@ -4,11 +4,44 @@
 
 using System;
 using System.Windows;
+using System.Windows.Controls;
+using AppModel;
+using AppViewModel;
 
 namespace AppView
 {
     public partial class WcWindow : Window
     {
-        public WcWindow() => InitializeComponent();
+        WcPresenter presenter;
+
+        public WcWindow()
+        {
+            InitializeComponent();
+        }
+
+        // This code-behind approach to supporting UpdateSourceTrigger=Explicit has drawbacks:
+        // - Such code-behind pollutes MVVM architecture.
+        // - Requires naming the control.
+        // - Doesn't scale well.
+        public void UpdateViewModel (object sender, EventArgs e)
+        {
+            inBox.GetBindingExpression (TextBox.TextProperty).UpdateSource();
+        }
+
+        // This is deemed acceptable code-behind because it is application startup.
+        private void WcController_Loaded (object sender, RoutedEventArgs e)
+        {
+            // Best practice is to create the model here - after the app has started.
+            // This allows showing splash or status while model is spinning up.
+            var model = new WcModel();
+
+            // DataContext can be created in XAML but only with the default constructor.
+            DataContext = presenter = new WcPresenter (model);
+
+            presenter.UpdateVM += UpdateViewModel;
+
+            // Process command line arguments:
+            presenter.InputLine = string.Join (" ", Environment.GetCommandLineArgs());
+        }
     }
 }
--- WcWpfMvvm/ViewModel/WcPresenter.cs
+++ WcWpfMvvm/ViewModel/WcPresenter.cs
@@ -7,12 +7,14 @@ using AppModel;
 
 namespace AppViewModel
 {
-    public class WcPresenter : WcModel
+    public class WcPresenter : Observable
     {
-        public WcModel WordCounter { get; private set; }
+        public event EventHandler UpdateVM;
+
+        private WcModel WordCounter;
+        public WcModelBind WC { get { return WordCounter.Bind; } }
 
         // Transient inputs like this do not belong in the model.
-        // TODO: Fix so this does not cause a heap allocation on EVERY KEYSTROKE.
         private string input = String.Empty;
         public string InputLine
         {
@@ -24,10 +26,10 @@ namespace AppViewModel
             }
         }
 
-        public WcPresenter()
+        public WcPresenter (WcModel model)
         {
-            WordCounter = new WcModel();
-            InputLine = String.Empty;
+            this.WordCounter = model;
+            this.InputLine = String.Empty;
         }
 
         public ICommand DoCountCommand
@@ -37,6 +39,9 @@ namespace AppViewModel
 
         private void CountWords()
         {
+            if (UpdateVM != null)
+                UpdateVM (this, EventArgs.Empty);
+
             if (! String.IsNullOrWhiteSpace (InputLine))
                 WordCounter.Parse (InputLine);
             InputLine = String.Empty;
