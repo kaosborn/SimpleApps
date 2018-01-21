63df5de9a8e3e40e62019a1a5621cd09a7a755cd
kaosborn <kaosborn@users.noreply.github.com>
2018-01-20 21:12:17 -0800

    add RelayCommand<T> class

--- WcWpfMvvm/Mvvw/RelayCommand.cs
+++ WcWpfMvvm/Mvvw/RelayCommand.cs
@@ -9,20 +9,63 @@ using System.Windows.Input;
 
 namespace Kaos.Mvvm
 {
+    // No parameter version.
     public class RelayCommand : ICommand
     {
-        private readonly Action action;
+        readonly Action action;
+        readonly Predicate<object> predicate;
 
-        public RelayCommand (Action action) => this.action = action;
+        public RelayCommand (Action action) : this (action, null)
+        { }
 
-        public void Execute (object parameter) => this.action();
+        public RelayCommand (Action action, Predicate<object> predicate)
+        {
+            if (action == null)
+                throw new ArgumentNullException (nameof (action));
+            this.action = action;
+            this.predicate = predicate;
+        }
 
-        public bool CanExecute (object parameter) => true;
+        // parameter is always null.
+        public bool CanExecute (object parameter) => predicate == null || predicate (parameter);
+
+        public void Execute (object parameter) => action();
 
         public event EventHandler CanExecuteChanged
         {
-            add { }
-            remove { }
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+
+
+    // Single parameter version.
+    public class RelayCommand<T> : ICommand
+    {
+        readonly Action<T> action;
+        readonly Predicate<T> predicate;
+
+        public RelayCommand (Action<T> action) : this (action, null)
+        {
+            this.action = action;
+        }
+
+        public RelayCommand (Action<T> action, Predicate<T> predicate)
+        {
+            if (action == null)
+                throw new ArgumentNullException (nameof (action));
+            this.action = action;
+            this.predicate = predicate;
+        }
+
+        public bool CanExecute (object parameter) => predicate == null || predicate ((T) parameter);
+
+        public void Execute (object parameter) => this.action ((T) parameter);
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
         }
     }
 }
