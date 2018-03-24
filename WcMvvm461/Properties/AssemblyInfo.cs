09e761d4dbbab27d97bc0c7ee08433ec8c184087
kaosborn <kaosborn@users.noreply.github.com>
2018-03-24 14:27:13 -0700

    add WcMvvm461 project

---
+++ WcMvvm461/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WcMvvm461")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleApps")]
+[assembly: AssemblyCopyright("Copyright © github.com/kaosborn 2018")]
+[assembly: AssemblyTrademark("freeware")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b2b4c754-c941-4c6b-ac42-bf4474298146")]
+
+[assembly: AssemblyVersion("0.1.3.0")]
+[assembly: AssemblyFileVersion("0.1.3.0")]
