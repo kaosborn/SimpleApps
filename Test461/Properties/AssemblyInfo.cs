6ef191d53cc6f245ad347ee791ddfe44645e274c
kaosborn <kaosborn@users.noreply.github.com>
2018-03-25 15:53:00 -0700

    add Test461 project

---
+++ Test461/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("Test461")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("SimpleApps")]
+[assembly: AssemblyCopyright ("Copyright © github.com/kaosborn 2018")]
+[assembly: AssemblyTrademark ("freeware")]
+[assembly: AssemblyCulture ("")]
+
+[assembly: ComVisible (false)]
+[assembly: Guid ("60a4a00a-4e5b-420c-8434-c63b0cd9cb5a")]
+[assembly: AssemblyVersion ("0.1.0.0")]
---
+++ Test461/Test_WcModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AppModel;
+using AppViewModel;
+
+namespace Test
+{
+    [TestClass]
+    public class Test_All
+    {
+        [TestMethod]
+        public void WcCtor()
+        {
+            var wc = new WcModel();
+            Assert.AreEqual (0, wc.Bind.History.Count);
+        }
+
+        [TestMethod]
+        public void WcParse()
+        {
+            var wc = new WcModel();
+            int k1 = wc.Parse ("a bb ccc");
+            Assert.AreEqual (3, k1);
+            Assert.AreEqual (1, wc.Bind.History.Count);
+        }
+
+        [TestMethod]
+        public void VmInput()
+        {
+            var wc = new WcModel();
+
+            var pr = new WcPresenter (wc);
+
+            Assert.AreEqual (0, pr.WC.History.Count);
+
+            wc.Parse ("aaa bb c");
+            Assert.IsTrue (pr.WC.History[0].StartsWith ("3 words"));
+        }
+    }
+}
